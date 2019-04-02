d7849fb7891c7304b046686ecb71765f640b1f31
айкеновад <@205_11>
2019-04-02 10:18:37 +0600

    -

---
+++ Counter/Counter.cpp
@@ -0,0 +1,48 @@
+﻿#include "Counter.h"
+
+Counter::Counter()
+{
+	cur_value = 0;//самому можно задать любое значение по умолчанию
+}
+
+Counter::Counter(int n)
+{
+	cur_value = n;
+}
+
+Counter::Counter(int c, int min, int max)
+{
+	min_value = min;
+	max_value = max;
+	if (c < min)
+		cur_value = 0;
+	else
+		cur_value = c % max;
+}
+
+void Counter::setMin(int n)
+{
+	min_value = n;
+}
+
+void Counter::setMax(int n)
+{
+	max_value = n;
+}
+
+void Counter::Init(int n) //устанавливаем с какой секунды начать отсчет
+{
+		cur_value = n % max_value;
+}
+
+void Counter::increaseValue()
+{
+	cur_value++;
+	if (cur_value == max_value) cur_value = 0;
+}
+
+int Counter::getCurrentValue()
+{
+	return cur_value;
+}
+
---
+++ Counter/Source.cpp
@@ -0,0 +1,29 @@
+﻿#include"Counter.h"
+
+int main() {
+	Counter c; //при таком объявлении объекта (без заданных параметров), вызывается констурктор по умолчанию
+	
+	c.setMin(0);
+	c.setMax(60);
+	c.Init(59);
+	c.increaseValue(); //увеличит на 1, и секундомер с 59 снова станет 0
+	cout << c.getCurrentValue() << endl;
+
+	c.Init(5);
+	c.increaseValue();
+	cout << c.getCurrentValue() << endl;
+
+	c.Init(63);
+	c.increaseValue();
+	cout << c.getCurrentValue() << endl;
+
+	Counter c1(15); //здесь вызывается конструктор с параметром
+	c1.increaseValue();
+	cout << c1.getCurrentValue() << endl;
+
+	Counter c2(95, 0, 60);
+	c2.increaseValue();
+	cout << c2.getCurrentValue() << endl;
+
+	system("pause");
+}
