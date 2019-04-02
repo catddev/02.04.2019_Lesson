a924bf4421bb502848fdee96c108d14b3db81d2c
айкеновад <@205_11>
2019-04-02 09:31:22 +0600

    Добавьте файлы проекта.

---
+++ Counter/Counter.h
@@ -0,0 +1,12 @@
+#pragma once
+//#include<iostream>
+
+class Counter {
+private: // спецификатор доступа, определ€ющий могут ли внешние функции видеть их, стоит по умолчанию
+	int min_value;
+	int max_value;
+	int cur_value;
+public: // писать об€зательно, нет по умолчанию
+
+
+};
